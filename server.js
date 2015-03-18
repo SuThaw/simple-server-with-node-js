abb40ac02046cc500bb45e13adb5540f5e5671f4
Su Thaw <suthaw524@gmail.com>
2015-03-18 16:37:21 +0630

    Cache System

--- server.js
+++ server.js
@@ -6,13 +6,28 @@ var mimeTypes = {
 	'.html': 'text/html',
 	'.css' : 'text/css'
 };
+var cache = {};
+
+function cacheAndDeliver(f,cb){
+	if(!cache[f]){
+		fs.readFile(f,function(err,data){
+			if(!err){
+				cache[f] = {content:data};
+			}
+			cb(err,data);
+		});
+		return;
+	}
+	console.log('loading ' + f + ' from cache');
+	cb(null,cache[f].content);
+}
 http.createServer(function(request,response){
 	var lookup = path.basename(decodeURI(request.url)) || 'index.html';
 	
 	var f = 'content/' + lookup;
 	fs.exists(f,function(exists){
 		if(exists){
-			fs.readFile(f,function(err,data){
+			cacheAndDeliver(f,function(err,data){
 				if(err) {
 					response.writeHead(500);
 					response.end('Server Error!');
